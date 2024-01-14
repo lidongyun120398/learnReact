6cf134d20ed98f95ff3d61adf20c2fb45faf4bc4
lidongyun120398 <1037083108@qq.com>
2024-01-14 14:52:19 +0800

    Compoent 和 PureComponent的区别

--- demo/src/views/Demo2.jsx
+++ demo/src/views/Demo2.jsx
@@ -7,6 +7,29 @@ import React from "react"
       + 如果经过浅比较，发现属性和状态并没有改变，则会返回false(也就是不继续更新组件)，有变化才会去更新
 */
 
+const isObject = function isObject(obj){
+  // return obj !== null && typeof obj === "object"
+  return obj !== null && /^(object|function)$/.test(typeof obj)
+}
+
+//浅比较两个对象
+const shallowEqual = function shallowEqual(objA,objB){
+  if(!isObject(objA) || !isObject(objB)) return false
+  if(objA === objB) return true
+  //先比较成员数量
+  let keysA = Reflect.ownKeys(objA)
+  let keysB = Reflect.ownKeys(objB)
+  if(keysA.length !== keysB.length) return false
+  //数量一致再逐一比较内部成员，且只比较第一级
+  for(let i = 0; i < keysA.length; i++){
+    let key = keysA[i];
+    //如果一个对象中有该成员，但是另外一个对象没有，或都有该成员，但是成员值不一致，都应该判定为不相同
+    if(!objB.hasOwnPropperty(key) || !Object.is(objA[key],objB[key])) return false
+  }
+  //以上都处理完，发现没有不相等的
+  return true;
+}
+
 class Demo extends React.PureComponent{
   state = {
     arr: [10, 20, 30]
@@ -29,14 +52,23 @@ class Demo extends React.PureComponent{
       <br />
       <button onClick={() => {
         arr.push(arr[arr.length - 1] + 10)
+        // this.setState({ arr }) //因为arr的栈地址没变，所以在shouldComponentUpdate中判断前后一致，不会变化
+        //想要更新的办法
+        //1. this.forceUpdate();
+        //2.
         this.setState({
-          arr
+          arr: [...arr] //浅拷贝，改变了arr的栈地址
         })
       }}>
         新增
       </button>
     </div>
   }
+
+  // shouldComponentUpdate(nextProps,nextState){
+  //   //props/state:修改前的属性状态
+  //   //nextProps/nextState:将要修改的属性状态
+  // }
 }
 
 export default Demo
