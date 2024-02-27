9c8c2c01f3f340ccc34592cbc8e7e224e9b78e43
lidongyun120398 <1037083108@qq.com>
2024-02-27 15:08:47 +0800

    useState及底层机制

--- hooks-demo/src/Demo1.jsx
+++ hooks-demo/src/Demo1.jsx
@@ -2,17 +2,28 @@ import React,{ useState } from 'react'
 import { Button } from 'antd'
 
 /* 
-
+ useState: React Hook函数之一，目的是在函数组件中使用状态，并且基于状态的修改可以让组件更新
+  let [num,setNum] = useState(initialValue)
+    + 执行useState，传递的initialValue是初始状态值
+    + 执行这个方法，返回结果是一个数组：[状态值，修改状态的方法]
+      + num存储的是获取的状态值
+      + setNum存储的是：修改状态的方法 
+    + 执行 setNum(value)
+      + 修改状态值为value
+      + 通知视图更新
+  函数组件[或者Hooks组件]不是类组件，所以没有实例的概念[调用组件不再是创建实例，而是把函数执行，产生一个私有上下文而已]，再所以，在函数组件中不涉及this的处理
 */
 
 const Demo1 = function Demo1(){
-  let n = 0
+  let [ num, setNum ] = useState(0)
+
+  const handle = () => {
+    setNum(num + 10);
+  }
 
   return <div className='demo'>
-    <span className="num">{ n }</span>
-    <Button type='primary' size='small' click={() => {
-      n += 10;
-    }}></Button>
+    <span className="num">{ num }</span>
+    <Button type='primary' size='small' click={handle}></Button>
   </div>
 }
 
---
+++ hooks-demo/src/Demo2.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { Button } from 'antd'
+
+/*
+  函数组件的每一次渲染(或更新)，都是把函数重新执行，产生一个全新的'私有上下文'
+    + 内部的代码都需要重新执行
+    + 涉及的函数需要重新的构建{这些函数的作用域(函数执行的上级上下文)，是每一次执行DEMO产生的闭包}
+    + 每一次执行DEMO函数，也会把useState重新执行，但是:
+      + 执行useState，只有第一次，设置的初始值会生效，其余以后再执行，获取的状态都是最新的状态值[而不是初始值]
+      + 返回的修改状态，每一次都是返回一个最新的
+*/
+
+const Demo2 = function Demo2(){
+  let [ num, setNum ] = useState(0)
+
+  const handle = () => {
+    setNum(num + 10);
+  }
+
+  return <div className='demo'>
+    <span className="num">{ num }</span>
+    <Button type='primary' size='small' click={handle}>新增</Button>
+  </div>
+}
+
+export default Demo2
