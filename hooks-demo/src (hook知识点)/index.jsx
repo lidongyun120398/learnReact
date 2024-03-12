730ae80be4b16eeea998cf1e8a50127c5d89139f
lidongyun120398 <1037083108@qq.com>
2024-03-12 16:59:29 +0800

    hook知识点存档

---
+++ hooks-demo/src (hook知识点)/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import Demo from './views/Demo3(useState执行时机)';
+import Demo from './views/Demo8(useLayoutEffect和useEffect的区别)';
+// import Vote from './views/Vote'
+
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ConfigProvider locale={zhCN}>
+    {/* <Vote title="React学好需要JS功底"/> */}
+    <Demo/>
+  </ConfigProvider>
+);
+
