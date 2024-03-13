c00440358e1c65982e5d59f0030585d4ac324efb
lidongyun120398 <1037083108@qq.com>
2024-03-13 12:23:02 +0800

    函数组件基于上下文方案实现祖先与后代通信(useContext)

--- hooks-demo/src/views/VoteMain.jsx
+++ hooks-demo/src/views/VoteMain.jsx
@@ -1,30 +1,25 @@
-import React from "react";
-import PropTypes  from "prop-types";
+import React,{ useContext } from "react";
+
 import ThemeContext from "../ThemeContext";
 
-class VoteMain extends React.Component {
-  static contextType = ThemeContext;
-    /* 属性规则校验 */
-    // static defaultProps = {
-    //     supNum: 0,
-    //     oppNum: 0
-    // };
-    // static propTypes = {
-    //     supNum: PropTypes.number,
-    //     oppNum: PropTypes.number
-    // }
+const VoteMain = function VoteMain() {
+    let {supNum,oppNum} = useContext(ThemeContext)
+    return <div className="main">
+             <p>支持人数:{supNum}人</p>
+             <p>反对人数:{oppNum}人</p>
+            </div>;
+   
 
-    render() {
-        let { supNum, oppNum } = this.context
-        let radios = '--',
-            total = supNum + oppNum;
-        if(total>0) radios = (supNum / total * 100).toFixed(2) + '%'
-        return <div className="main">
-            <p>支持人数:{supNum}人</p>
-            <p>反对人数:{oppNum}人</p>
-            <p>支持比率:{radios}</p>
-        </div>;
-    }
-}
+    // return <ThemeContext.Consumer>
+    //   {context => {
+    //     let {supNum,oppNum} = context
+    //     return <div className="main">
+    //             <p>支持人数:{supNum}人</p>
+    //             <p>反对人数:{oppNum}人</p>
+    //           </div>;
+    //   }}
+    // </ThemeContext.Consumer>
+};
+
 
 export default VoteMain;
