8268db161f221faa592a6d78627d624eb59aee49
lidongyun120398 <1037083108@qq.com>
2024-03-19 22:19:35 +0800

    分支初始化

--- hooks-demo/src/index.jsx
+++ hooks-demo/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'
+
 
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN'
@@ -9,7 +9,7 @@ import zhCN from 'antd/locale/zh_CN'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ConfigProvider locale={zhCN}>
-    <App />
+    
   </ConfigProvider>
 );
 
