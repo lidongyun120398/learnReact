5f27186d6ddf498db1c6bd679d0a0d097b5b2c46
lidongyun120398 <1037083108@qq.com>
2024-03-20 23:02:20 +0800

    redux基础操作和思想

--- hooks-demo/src/views/Vote.jsx
+++ hooks-demo/src/views/Vote.jsx
@@ -4,24 +4,14 @@ import VoteMain from './VoteMain';
 import VoteFooter from './VoteFooter';
 
 const Vote = function Vote() {
-    let [supNum,setSupNum] = useState(10),
-        [oppNum,setOppNum] = useState(0)
-
-    const change = useCallback(type => {
-        if(type === 'sup'){
-            setSupNum(supNum + 1)
-            return
-        }
-        setOppNum(oppNum + 1)
-    },[supNum,oppNum])
-
+   
     return <div className="vote-box">
         <div className="header">
             <h2 className="title">React是很棒的前端框架</h2>
-            <span className="num">{supNum + oppNum}</span>
+            <span className="num">0</span>
         </div>
-        <VoteMain supNum={supNum} oppNum={oppNum}/>
-        <VoteFooter change={change}/>
+        <VoteMain/>
+        <VoteFooter/>
     </div>;
 };
 
--- hooks-demo/src/views/VoteFooter.jsx
+++ hooks-demo/src/views/VoteFooter.jsx
@@ -2,15 +2,13 @@ import React, { memo } from "react";
 import { Button } from 'antd';
 import PropTypes from 'prop-types'
 
-const VoteFooter = function VoteFooter(props) {
-    let {change} = props
+const VoteFooter = function VoteFooter() {
+    
     return <div className="footer">
-        <Button type="primary" onClick={change.bind(null,'sup')}>支持</Button>
-        <Button type="primary" danger onClick={change.bind(null,'opp')}>反对</Button>
+        <Button type="primary">支持</Button>
+        <Button type="primary" danger>反对</Button>
     </div>;
 };
 
-VoteFooter.propTypes = {
-    change:PropTypes.func.isRequired
-}
+
 export default memo(VoteFooter);
